2fbf3aec5d1db944027dabe1b3334ae5ec0e936d
emalaquin2022 <emalaquin2022@46SE48-C4JMYJ2.ad.campus-eni.fr>
2023-01-13 10:03:37 +0100

    ajout de l'implementation article DAO

--- src/main/java/fr/eni/ecole/encheres/dal/DAOFactory.java
+++ src/main/java/fr/eni/ecole/encheres/dal/DAOFactory.java
@@ -5,4 +5,7 @@ public class DAOFactory {
 	public static DAOUtilisateur getDAOUtilisateur () {
 		return new UtilisateurDAOJdbcImpl ();
 	}
+	public static DAOArticleVendu getDAOArticle () {
+		return new ArticleDAOJdbcImpl ();
+	}
 }
